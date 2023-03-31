40fd42c1a9bc7914b52dc9474927fd9a33842b75
ash-hashtag <hashtag438@gmail.com>
2023-03-31 12:58:39 +0530

    fixed bug: doesn't work if itemCount is less than itemExtent

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,22 +1,60 @@
-import { ListView } from "vanilla-list-view"
+// import { ListView } from "vanilla-list-view"
+import { ListView } from "../../src/index";
 
 const colors = ["blue", "yellow", "red", "purple", "green", "orange"]
 
-const listView = new ListView({
-    itemBuilder: (idx) => {
-        const div = document.createElement("div")
-        div.innerText = `Hello ${idx} ${Math.random()}`
-        div.style.height = `${100 + (Math.random() * 400) }px`
-        div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+let itemCount = 11
 
-        return div
-    },
-    itemCount: 100,
-    numOfItemsCanBeModifiedAtOnce: 5,
-    numOfItemsToRender: 20,
-    cacheLimit: 50,
-    triggerIndexToUpdateElements: 5,
-    height: "50vh"
+const listView = new ListView({
+  itemBuilder: (idx) => {
+    const div = document.createElement("div")
+    div.innerText = `Hello ${idx} ${Math.random()}`
+    div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+    
+    const img = document.createElement("img")
+    const dimensions = {
+      width: Math.floor(Math.random()*100 + 100),
+      height: Math.floor(Math.random() * 100 + 100)
+    }
+    
+    // div.style.height = `${100 + (Math.random() * 400)}px`
+    img.src = `https://picsum.photos/${dimensions.width}/${dimensions.height}`
+
+    div.appendChild(img)
+
+    return div
+  },
+  itemCount: itemCount,
+  numOfItemsCanBeModifiedAtOnce: 5,
+  numOfItemsToRender: 20,
+  cacheLimit: 100,
+  triggerIndexToUpdateElements: 5,
+  height: "80vh"
 })
 
-document.body.appendChild(listView)
+
+const itemCountElem = document.createElement("div")
+itemCountElem.textContent = itemCount.toString()
+
+
+const incrementButton = document.createElement("button")
+
+incrementButton.textContent = "increment + 10"
+
+incrementButton.addEventListener("click", () => {
+  itemCount += 10
+  itemCountElem.textContent = itemCount.toString()
+  listView.updateItemCount(itemCount)
+})
+const decrementButton = document.createElement("button")
+
+decrementButton.textContent = "decrement - 10"
+
+decrementButton.addEventListener("click", () => {
+  itemCount -= 10
+  listView.updateItemCount(itemCount)
+  itemCountElem.textContent = itemCount.toString()
+})
+
+document.body.append(incrementButton, itemCountElem, decrementButton, listView)
+
